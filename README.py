b51d193df7a0a112a60ad2e8da91c7c93428d30a
Mohamed Attar <93054654+MohammedElattar@users.noreply.github.com>
2024-02-16 11:33:53 +0200

    Update README.py

--- README.py
+++ README.py
@@ -1,10 +1,182 @@
-- 👋 Hi, I’m @MohammedElattar
-- 👀 I’m Laravel Backend Develpoer
-- 🌱 I’m currently working with Laravel framework
-- 💞️ I’m currently working as Backend Developer
-- 📫 How to reach me mohammedattar0100020@gmail.com
+<h1 align="center">Hi 👋, I'm Mohamed Attar</h1>
+<h3 align="center">A Laravel Developer from Egypt</h3>
 
-<!---
-MohammedElattar/MohammedElattar is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+![GitHub followers](https://img.shields.io/github/followers/MohammedElattar?style=social)
+![Twitter Follow](https://img.shields.io/twitter/follow/MohammedElattar?style=social)
+
+- 🔭 I’m currently working on **Doctor Code**
+
+- 🌱 I’m currently learning **Design Patterns and Devops**
+
+- 📫 How to reach me **mohammedattar0100020@gmail.com**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://twitter.com/ahmed3id2001" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="ahmed3id2001" height="30" width="40" /></a>
+<a href="https://www.linkedin.com/in/mohamed-attar-backend" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="ahmed-eid-568109144" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> 
+  <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> 
+  </a> 
+  <a href="https://firebase.google.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" width="40" height="40"/> 
+  </a> 
+  <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> 
+    <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> 
+  </a> 
+  <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> 
+  </a> 
+  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> 
+  </a> 
+  <a href="https://laravel.com/" target="_blank" rel="noreferrer"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-plain-wordmark.svg" alt="laravel" width="40" height="40"/> 
+  </a> 
+  <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/> 
+  </a> 
+  <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/> 
+  </a> 
+  <a href="https://www.nginx.com" target="_blank" rel="noreferrer"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg" alt="nginx" width="40" height="40"/> 
+  </a> 
+  <a href="https://www.php.net" target="_blank" rel="noreferrer"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="40" height="40"/> 
+  </a> 
+  <a href="https://postman.com" target="_blank" rel="noreferrer"> 
+    <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40"/> 
+  </a> 
+  <a href="https://www.python.org" target="_blank" rel="noreferrer"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> 
+  </a> 
+</p>
+
+<div align="center">
+  <a href="">
+  <img  src="https://github.com/ahmedeid46/ahmedeid46/blob/main/resources/img/grid-snake.svg"
+       alt="snake" /></a>
+</div>
+
+-----
+<details>
+  <summary>☎️ contact me</summary>
+<div>
+  <samp>
+    <h2 align="center">you can reach me by:</h2>
+    <p align="center">
+      <br/>
+      <a href="hhttps://www.linkedin.com/in/mohamed-attar-backend" target="blank"><img align="center"
+         src="https://img.shields.io/badge/linkedin-%231DA1F2.svg?style=for-the-badge&logo=linkedin&logoColor=white"
+         alt="ahmedeid4601" height="30"/></a>
+      <a href="https://www.facebook.com/Mohamed.Ahmed.Attar" target="blank"><img align="center"
+         src="https://img.shields.io/badge/facebook-4267B2.svg?style=for-the-badge&logo=facebook&logoColor=white"
+         alt="ahmedeid" height="30"/></a>
+      <a href="mailto:mohammedattar0100020@gmail.com" target="blank"><img align="center"
+         src="https://img.shields.io/badge/gmail-EA4335.svg?style=for-the-badge&logo=gmail&logoColor=white"
+         alt="azzar" height="30"/></a>
+    </p>
+  <p align="center">
+      <a href="https://wa.me/+201006131248" target="blank"><img align="center"
+         src="https://img.shields.io/badge/whatsapp-4B7F1.svg?style=for-the-badge&logo=whatsapp&logoColor=white"
+         alt="ahmedeid" height="30"/></a>
+      <br>
+    </p>
+  </samp>
+</div>
+</details>
+
+<details>
+  <summary>🧮 about</summary>
+<div>
+<samp>
+<h2 align="center">About this Account</h2>
+ <p align="center">
+  <a href="github.com/MohammedElattar" target="blank"><img align="center" 
+     src="https://komarev.com/ghpvc/?username=ahmedeid46&style=for-the-badge&label=PROFILE+VIEWS" height="25"
+     alt="views count" /></a>
+  <p align="center">
+  <a href="https://www.codefactor.io/repository/github/ahmedeid46/egyptian-einvoicing-ereceipt"><img align="center"
+     src="[https://www.codefactor.io/repository/github/1999azzar/1999azzar/badge/main](https://www.codefactor.io/repository/github/ahmedeid46/egyptian-einvoicing-ereceipt/badge)" height="25"
+     alt="CodeFactor" /></a>
+  
+  </p>
+ 
+  <a href="github.com/MohammedElattar"><img align="center"
+     src="https://forthebadge.com/images/badges/works-on-my-machine.svg" height="25"
+     alt="work on my machine" /></a>
+ </p>
+ </samp>
+</div>
+</details>
+  
+<details> 
+  <summary>💻 GitHub Profile Stats</summary>
+  <div>
+  <samp>
+    <h2 align="center"> Github stats </h2>
+      <br/>
+    <details open>
+  <summary><h3>Languages</h3></summary>
+            <p align="center">
+        <a href="https://github.com/MohammedElattar/">
+          <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=MohammedElattar&langs_count=6&theme=gruvbox&layout=compact&hide_border=true"
+          alt="MohammedElattar :: overall Top Langs " /></a>
+      </p>
+        <p align="center">
+          <a href="https://github.com/MohammedElattar/">
+          <img width="45%" src="https://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=MohammedElattar&theme=gruvbox&layout=compact&hide_border=true"
+          alt="MohammedElattar :: Top Langs by repo" />
+          <img width="45%" src="https://github-profile-summary-cards.vercel.app/api/cards/most-commit-language?username=MohammedElattar&theme=gruvbox&layout=compact&hide_border=true"
+          alt="MohammedElattar :: Top Langs by commit" />
+          </a>
+        </p>
+</details>
+    <details open>
+  <summary><h3>stasistic</h3></summary>
+        <p align="center">
+          <a href="https://github.com/MohammedElattar/">
+          <img width="49.5%" src="https://github-readme-stats.vercel.app/api?username=MohammedElattar&show_icons=true&theme=gruvbox&hide_border=true" />
+          <img width="49.5%" src="https://github-readme-streak-stats.herokuapp.com/?user=MohammedElattar&theme=gruvbox&hide_border=true" />
+          </a>
+       </p>
+     <br>
+     </samp>
+  </div>    
+</details>
+  
+<details>
+  <summary>🌏 global</summary>
+  <br/>
+  <details open>
+  <summary>👷‍♂️ create your own custom badge</summary>
+  <div>
+  <samp>
+    <h2 align="center">u can try using these website for creating your own custom badge</h2>
+    <p align="center">
+      <a href="https://forthebadge.com/generator/" target="blank">
+        <img src="https://forthebadge.com/images/mark.svg" img align="center" height="50"
+        alt="for the badge"/></a>        
+      <a href="https://badgen.net/" target="blank">
+        <img src="https://badgen.net/static/favicon.png" img align="center" height="50"
+        alt="badgen"/></a>
+      <a href="https://shields.io/" target="blank">
+        <img src="https://raw.githubusercontent.com/badges/shields/master/readme-logo.svg" img align="center" height="50"
+        alt="shields.io"/></a>
+    </p>
+    </samp>
+  </div>
+</details> 
+<br/>
+</details> 
+
+<summary><b>⚡ Recent GitHub Activity</b></summary>
+  <br/>
+   <a href="https://github.com/MohammedElattar"><img alt="7oSkaaa's Activity Graph" src="https://activity-graph.herokuapp.com/graph?username=MohammedElattar&custom_title=MohammedElattar's%20Contribution%20Graph&theme=react-dark" /></a>
+  <br/>
+
+## 🏆GitHub Trophies
+![](https://github-profile-trophy.vercel.app/?username=ahmedeid46&theme=onedark&no-frame=true&no-bg=true&margin-w=4)
+
